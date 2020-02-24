f9d0453bed5a94a787b5d683dccf1bdc7999ed51
zhan-hc <zhan19527@gmail.com>
2020-02-24 23:23:15 +0800

    add header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+      <home-header></home-header>
+  </div>
+</template>
+
+<script type="text/javascript">
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped ref="stylesheet/stylus">
+</style>
