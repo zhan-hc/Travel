5aed8517356aabd8607476b68dc8ffefa8106439
zhan-hc <zhan19527@gmail.com>
2020-02-29 22:06:31 +0800

    index finish

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="city">
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'city',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
